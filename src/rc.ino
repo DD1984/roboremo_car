fff210d0d08189c8bebff83c676119aaaf8f16f0
konev <konev@konev>
2018-09-21 18:16:24 +0300

    add connection checking

--- src/rc.ino
+++ src/rc.ino
@@ -13,9 +13,13 @@
 
 #define RESXu 1024u
 
-#define SSID "car 2.0"
-#define PSK ""
+#define WIFI_SSID "mywifi"
+#define WIFI_PSK ""
+#define WIFI_CHANNEL 1
+
 #define PORT 9876
+
+#define CON_CHECK_INTERVAL 1000 //ms
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
@@ -45,15 +49,7 @@ ctrl_t controls[] = {
 
 Servo servo;
 
-WiFiClient client;
 WiFiUDP udp;
-
-
-
-unsigned long lastCmdTime = 60000;
-unsigned long aliveSentTime = 0;
-
-
 
 int16_t calc100to256(int8_t x) // return x*2.56
 {
@@ -71,10 +67,10 @@ int16_t calc100toRESX(int8_t x) // return x*10.24
 	return res;
 }
 
-int16_t calcRESXtoServo(int16_t x)  // return x/1.024
+int16_t calcRESX1000(int16_t x)  // return x/1.024
 {
 // *1000/1024 = x - x/32 + x/128
-	return ((x - (x >> 5) + (x >> 7) + 1000) >> 1) + 1000;
+	return x - (x >> 5) + (x >> 7);
 }
 
 // input parameters;
@@ -118,15 +114,6 @@ int expo(int x, int k)
 	return neg ? -y : y;
 }
 
-int16_t in2out(int16_t x)
-{
-	return calcRESXtoServo(expo(calc100toRESX(x), servo_exp * 10));
-}
-
-void init_gpios(void)
-{
-}
-
 void setup()
 {
 	delay(1000);
@@ -136,13 +123,13 @@ void setup()
 	IPAddress ip(192, 168, 0, 1); // From RoboRemo app, connect to this IP
 	IPAddress netmask(255, 255, 255, 0);
 
-	WiFi.softAPConfig(ip, ip, netmask); // configure ip address for softAP 
-	WiFi.softAP(SSID, PSK); // configure ssid and password for softAP
+	WiFi.softAPConfig(ip, ip, netmask);
+	WiFi.softAP(WIFI_SSID, WIFI_PSK, WIFI_CHANNEL, 0, 1); // 1 client possible
 
 	udp.begin(PORT); // start UDP server
 
-	Serial.println("ESP8266 RC receiver 1.1 powered by RoboRemo");
-	Serial.println((String)"SSID: " + SSID + "  PASS: " + PSK);
+	Serial.println("ESP8266 RC receiver (UDP) 1.0 powered by RoboRemo");
+	Serial.println((String)"SSID: " + WIFI_SSID + "  PASS: " + WIFI_PSK);
 	Serial.println((String)"RoboRemo app must connect to " + ip.toString() + ":" + PORT);
 }
 
@@ -179,26 +166,107 @@ int parse_pkt(char *pkt_buf)
 	return UNKNOWN;
 }
 
+enum {
+	CON_FAIL,
+	CON_WIFI,
+	CON_WIFI_DATA,
+};
+
+int con_state = CON_FAIL;
+
+void start_ctrl(void)
+{
+	Serial.printf("start\n");
+	servo.attach(4, 1000, 2000);
+
+}
+
+void stop_ctrl(void)
+{
+	Serial.print("stop\n");
+	servo.detach();
+}
+
+
 void loop()
 {
+	static int last_time = 0;
+	static bool data_available = false;
+
+	int now_time = millis();
+	if (now_time - last_time > CON_CHECK_INTERVAL) {
+
+		//Serial.print("check connection\n");
+
+		last_time = now_time;
+
+		static int last_sta_num = 0;
+		int cur_sta_num = WiFi.softAPgetStationNum();
+
+		if (cur_sta_num > 0 && last_sta_num == 0) {
+			Serial.print("client connected\n");
+			con_state = CON_WIFI;
+		}
+		if (cur_sta_num == 0 && last_sta_num > 0) {
+			Serial.print("client disconnected\n");
+			if (con_state == CON_WIFI_DATA)
+				stop_ctrl();
+			con_state = CON_FAIL;
+		}
+
+		last_sta_num = cur_sta_num;
+
+		if (con_state != CON_FAIL) {
+			static bool last_data_available = false;
+
+			if (con_state == CON_WIFI && !last_data_available && data_available) {
+				Serial.print("client data available\n");
+				con_state = CON_WIFI_DATA;
+				start_ctrl();
+			}
+
+			if (con_state == CON_WIFI_DATA && last_data_available && !data_available) {
+				Serial.print("client data timeot expired\n");
+				con_state = CON_WIFI;
+				stop_ctrl();
+			}
+
+			last_data_available = data_available;
+			data_available = false;
+		}
+	}
+
+	if (con_state == CON_FAIL)
+		return;
+
 	int pkt_size = udp.parsePacket();
 	if (pkt_size) {
 		//Serial.printf("Received %d bytes from %s, port %d\n", pkt_size, udp.remoteIP().toString().c_str(), udp.remotePort());
 		char pkt_buf[64];
 		int len = udp.read(pkt_buf, sizeof(pkt_buf));
 		if (len > 0) {
+			//Serial.printf("UDP packet contents: %s\n", pkt_buf);
+
+			data_available = true;
+
+			if (con_state == CON_WIFI)
+				return;
+
 			pkt_buf[len] = 0;
 
 			int id = parse_pkt(pkt_buf);
 			ctrl_t *ctrl = get_ctrl(id);
+
 			if (ctrl)
 				Serial.printf("ctrl: \"%s\" val: %d\n", ctrl->roboremo_id, ctrl->val);
 
+			int16_t servo_val;
+
 			switch (id) {
 				case SERVO:
-					if (!servo.attached())
-						servo.attach(4, 1000, 2000);
-					servo.writeMicroseconds(in2out(ctrl->val));
+					servo_val = ((calcRESX1000(expo(calc100toRESX(ctrl->val), servo_exp * 10)) + 1000) >> 1) + 1000;
+					Serial.printf("servo_val: %d\n", servo_val);
+					servo.writeMicroseconds(servo_val);
 				break;
 				case SERVO_EXP:
 				break;
@@ -207,41 +275,10 @@ void loop()
 			}
 		}
 
-		//Serial.printf("UDP packet contents: %s\n", pkt_buf);
 
 	}
 
 #if 0
-  // if contact lost for more than half second
-  if(millis() - lastCmdTime > 500) {  
-    for(int i=0; i<chCount; i++) {
-      // set all values to middle
-      servoCh[i].writeMicroseconds(1500);
-      servoCh[i].detach(); // stop PWM signals
-    }
-  }
-
-  
-  if(!client.connected()) {
-    client = server.available();
-    return;
-  }
-
-  // here we have a connected client
-
-  if(client.available()) {
-    char c = (char)client.read(); // read char from client (RoboRemo app)
-
-    if(c=='\n') { // if it is command ending
-      cmd[cmdIndex] = 0;
-      exeCmd();  // execute the command
-      cmdIndex = 0; // reset the cmdIndex
-    } else {      
-      cmd[cmdIndex] = c; // add to the cmd buffer
-      if(cmdIndex<99) cmdIndex++;
-    }
-  } 
-
   if(millis() - aliveSentTime > 500) { // every 500ms
     client.write("alive 1\n");
     // send the alibe signal, so the "connected" LED in RoboRemo will stay ON
